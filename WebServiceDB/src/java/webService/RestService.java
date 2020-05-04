a5d08c863e936381765250bd4ff06eb8fc6c09de
Sean Deely <40064774+seandeelyy@users.noreply.github.com>
2020-05-04 14:51:57 +0100

    Fresh Start (Again)

---
+++ WebServiceDB/src/java/webService/RestService.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package webService;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Application;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+
+@Path("/MyRestService")
+@ApplicationPath("/resources")
+public class RestService extends Application {
+
+	// http://localhost:8080/WebServiceDB/resources/MyRestService/sayHello
+	@GET
+	@Path("/sayHello")
+	public String getHelloMsg() {
+		return "Hello World";
+	}
+
+	@GET
+	@Path("/echo")
+	public Response getEchoMsg(@QueryParam("message") String msg) {
+		return Response.ok("Your message was: " + msg).build();
+	}
+        
+        @GET
+	@Path("/object")
+	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+	public SimpleObject getObject() {
+		return new SimpleObject(1, "Test");
+	}
+
+}
